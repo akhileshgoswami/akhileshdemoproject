7e451d3d749f3c12a8c85a80bc4aaa261adf3bef
Akhilesh Goswami <akgileshgoswami@gmail.com>
2024-03-19 09:39:58 +0530

    updated code

--- akhileshdemoproject/urls.py
+++ akhileshdemoproject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings # new
 from  django.conf.urls.static import static #new
-from . import views
+from akhileshdemoproject import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
